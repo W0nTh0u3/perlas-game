0b9e478c4ba2fa449a4fa8443e13b93d28b89e40
Ryan Emmanuel SJ Verzo <ryanemmanuelvrz345@gmail.com>
2019-12-09 21:35:04 +0800

    Minor - Added Table for Baybayins
    Added Table for Baybayins also added quit confirmation and also revamped menu design again.

---
+++ Assets/MainMenuOnlyScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuOnlyScene : MonoBehaviour
+{
+    public GameObject quitCanvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            quitCanvas.SetActive(true);
+        }
+    }
+    public void QuitGameNow()
+    {
+        Application.Quit();
+        Debug.Log("game quit.");
+    }
+}
